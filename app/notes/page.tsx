2589bd7b65918e7b92fe276f2c631a3b20a78b4d
seroga <sergijvchichur@gmail.com>
2026-07-31 00:43:03 +0300

    created client part of the main code

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,86 +1,18 @@
-'use client';
-
-import css from '@/app/notes/page.module.css';
-import { useState } from 'react';
-import { useQuery, keepPreviousData } from '@tanstack/react-query';
-import { useDebouncedCallback } from 'use-debounce';
-
-import NoteList from '@/components/NoteList/NoteList';
-import Pagination from '@/components/Pagination/Pagination';
-import Modal from '@/components/Modal/Modal';
-import NoteForm from '@/components/NoteForm/NoteForm';
-import SearchBox from '@/components/SearchBox/SearchBox';
+import { QueryClient, HydrationBoundary, dehydrate } from '@tanstack/react-query';
 import { fetchNotes } from '@/lib/api';
+import NotesClient from './Notes.client';
 
-function Notes() {
-  const [currentPage, setCurrentPage] = useState(1);
-  const [isModalOpen, setIsModalOpen] = useState(false);
+export default async function NotesPage() {
+  const queryClient = new QueryClient();
 
-  const [searchInputValue, setSearchInputValue] = useState('');
-  const [searchQuery, setSearchQuery] = useState('');
-
-  const openModal = () => setIsModalOpen(true);
-  const closeModal = () => setIsModalOpen(false);
-
-  const debouncedSearch = useDebouncedCallback((value: string) => {
-    setSearchQuery(value);
-    setCurrentPage(1);
-  }, 500);
-
-  // Обробник зміни значення в SearchBox
-  const handleSearchChange = (value: string) => {
-    setSearchInputValue(value);
-    debouncedSearch(value);
-  };
-
-  const { data, isSuccess, isLoading, isError } = useQuery({
-    queryKey: ['notes', currentPage, searchQuery],
-    queryFn: () =>
-      fetchNotes({
-        page: currentPage,
-        search: searchQuery,
-      }),
-    placeholderData: keepPreviousData,
+  await queryClient.prefetchQuery({
+    queryKey: ['notes', 1, ''],
+    queryFn: () => fetchNotes({ page: 1, search: '' }),
   });
 
-  const totalPages = data?.totalPages ?? 0;
-
   return (
-    <div className={css.app}>
-      <header className={css.toolbar}>
-        <SearchBox value={searchInputValue} onChange={handleSearchChange} />
-
-        {isSuccess && totalPages > 1 && (
-          <Pagination
-            totalPages={totalPages}
-            currentPage={currentPage}
-            onPageChange={setCurrentPage}
-          />
-        )}
-
-        <button onClick={openModal} className={css.button}>
-          Create note +
-        </button>
-      </header>
-
-      <main className={css.main}>
-        {isLoading && <p>Loading notes...</p>}
-        {isError && <p>Error loading notes.</p>}
-
-        {data && data.notes.length > 0 ? (
-          <NoteList notes={data.notes} />
-        ) : (
-          isSuccess && <p>No notes found.</p>
-        )}
-      </main>
-
-      {isModalOpen && (
-        <Modal onClose={closeModal}>
-          <NoteForm onClose={closeModal} />
-        </Modal>
-      )}
-    </div>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <NotesClient />
+    </HydrationBoundary>
   );
 }
-
-export default Notes;
